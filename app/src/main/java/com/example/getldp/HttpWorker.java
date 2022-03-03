3537fd3574e53f9e0406cd0a8f77e699f7aa5863
= <toon.dehaene@student.kuleuven.be>
2022-03-03 18:11:04 +0100

    simple icon

--- app/src/main/java/com/example/getldp/HttpWorker.java
+++ app/src/main/java/com/example/getldp/HttpWorker.java
@@ -180,16 +180,14 @@ public class HttpWorker extends Worker {
     private void createNotificationChannel() {
         // Create the NotificationChannel, but only on API 26+ because
         // the NotificationChannel class is new and not in the support library
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = getApplicationContext().getString(R.string.channel_name);
-            String description = getApplicationContext().getString(R.string.channel_description);
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
-            channel.setDescription(description);
-            // Register the channel with the system; you can't change the importance
-            // or other notification behaviors after this
-            NotificationManager notificationManager = getApplicationContext().getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-        }
+        CharSequence name = getApplicationContext().getString(R.string.channel_name);
+        String description = getApplicationContext().getString(R.string.channel_description);
+        int importance = NotificationManager.IMPORTANCE_DEFAULT;
+        NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+        channel.setDescription(description);
+        // Register the channel with the system; you can't change the importance
+        // or other notification behaviors after this
+        NotificationManager notificationManager = getApplicationContext().getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(channel);
     }
 }
